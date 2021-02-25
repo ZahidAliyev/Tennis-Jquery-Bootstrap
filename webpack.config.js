5978912bbd9d8b4bfeea167f2f95c0f9b2f8f95a
Zahid Aliyev <hermitaliyev96@gmail.com>
2021-02-25 18:50:23 +0400

    Webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
     entry: './src/tennis/main.js',
     output: {
-        path: path.resolve(__dirname, '/public/tennis'),
+        path: path.resolve(__dirname, 'public/tennis'),
         filename: 'main.js',
     }
 };
